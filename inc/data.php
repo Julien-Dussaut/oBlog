29ceca08c16fbbb1c545bc12fc42410cb0c87704
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2024-03-11 14:48:43 +0100

    Méga bonus fini.

--- inc/data.php
+++ inc/data.php
@@ -61,55 +61,56 @@ $dataAuthorsList = [
 -----------------------------------------------------------------------------------------
 */
 
-// Les différents articles sous forme d'objet
-$dataArticlesList = [
-    1 => new Article(
-        'Ivre, il refait tous les challenges en un week-end sans dormir.',
-        'Ou comment j\'ai appris plein de choses en faisant une nouvelle fois tous les challenges que j\'avais loupé.',
-        new Author('Alexandre', 3),
-        '2017-07-13',
-        new Category('Ma Vie De Dev', 4) 
-    ),
-    2 => new Article(
-        'POO or not POO, that is the question.',
-        'La POO est-elle vraiment indispensable pour une architecture solide ? Etude de cas avec PHP.',
-        new Author('Julie', 5),
-        '2017-07-04',
-        new Category('TeamBack', 1)
-    ),
-    3 => new Article(
-        'Git, pour les n00bs.',
-        'Un p\'tit récap rapide pour tout ceux qui, comme moi, ont galéré sur ce magnifique outil de versionning.',
-        new Author('Lucie', 6),
-        '2017-06-19',
-        new Category('Collaboration', 3)
-    ),
-    4 => new Article(
-        'Le syndrome de la page blanche',
-        'Cette frustration quand le code ne vient pas, le temps passe, la deadline approche...',
-        new Author('Xavier', 7),
-        '2017-05-06',
-        new Category('Ma Vie De Dev', 4)
-    ),
-];
-
 // Les catégories
 $dataCategoriesList = [
     // ID => objet Category
-    1 => new Category('TeamBack'),
-    2 => new Category('TeamFront'),
-    3 => new Category('Collaboration'),
-    4 => new Category('Ma Vie De Dev')
+    1 => new Category('TeamBack', 1),
+    2 => new Category('TeamFront', 2),
+    3 => new Category('Collaboration', 3),
+    4 => new Category('Ma Vie De Dev', 4)
 ];
 
 // Les auteurs
 $dataAuthorsList = [
     // ID => objet Author
-    1 => new Author('Maxime'),
-    2 => new Author('Anthony'),
-    3 => new Author('Alexandre'),
-    4 => new Author('Dario'),
-    5 => new Author('Julie'),
-    6 => new Author('Lucie'),
-    7 => new Author('Xavier')
+    1 => new Author('Maxime', 1),
+    2 => new Author('Anthony', 2),
+    3 => new Author('Alexandre', 3),
+    4 => new Author('Dario', 4),
+    5 => new Author('Julie', 5),
+    6 => new Author('Lucie', 6),
+    7 => new Author('Xavier', 7)
 ];
+
+// Les différents articles sous forme d'objet
+$dataArticlesList = [
+    1 => new Article(
+        'Ivre, il refait tous les challenges en un week-end sans dormir.',
+        'Ou comment j\'ai appris plein de choses en faisant une nouvelle fois tous les challenges que j\'avais loupé.',
+        $dataAuthorsList[3],
+        '2017-07-13',
+        $dataCategoriesList[4]
+    ),
+    2 => new Article(
+        'POO or not POO, that is the question.',
+        'La POO est-elle vraiment indispensable pour une architecture solide ? Etude de cas avec PHP.',
+        $dataAuthorsList[5],
+        '2017-07-04',
+        $dataCategoriesList[1]
+    ),
+    3 => new Article(
+        'Git, pour les n00bs.',
+        'Un p\'tit récap rapide pour tout ceux qui, comme moi, ont galéré sur ce magnifique outil de versionning.',
+        $dataAuthorsList[6],
+        '2017-06-19',
+        $dataCategoriesList[3]
+    ),
+    4 => new Article(
+        'Le syndrome de la page blanche',
+        'Cette frustration quand le code ne vient pas, le temps passe, la deadline approche...',
+        $dataAuthorsList[7],
+        '2017-05-06',
+        $dataCategoriesList[4]
+    ),
+];
+
